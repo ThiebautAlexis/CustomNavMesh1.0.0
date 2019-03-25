cf527fa47c3e0b410bf98c429a6fbd2748c576f6
Alexis <thiebautalexis01@gmail.com>
2019-03-25 16:30:08 +0100

    add set destination class

---
+++ Assets/SetDestination.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDestination : MonoBehaviour
+{
+
+    [SerializeField] CustomNavMeshAgent agent;  
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            RaycastHit _hitInfo; 
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hitInfo))
+            {
+                agent.SetDestination(_hitInfo.point); 
+            }
+        }
+	}
+}
